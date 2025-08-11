73c348fa9a965d8855dd95b7b6cd2edd8131ba78
Reem Ben-David <reemile1996@gmail.com>
2025-08-11 13:17:09 +0300

    feat(app): Connect the http client to the app

--- apps/chatBHP/src/app/app.config.ts
+++ apps/chatBHP/src/app/app.config.ts
@@ -4,12 +4,18 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
 import { appRoutes } from './app.routes';
+import { API_URL } from '@chat-bhp/core/data-access';
+import { environment } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(),
+    { provide: API_URL, useValue: environment.API_URL },
   ],
 };
