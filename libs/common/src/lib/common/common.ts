4a575aa7535a79588f2c5748478b2cbf6b5a25e3
Reem Ben-David <reemile1996@gmail.com>
2025-08-11 23:22:53 +0300

    feat(common): Init common lib

---
+++ libs/common/src/lib/common/common.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-common',
+  imports: [CommonModule],
+  templateUrl: './common.html',
+  styleUrl: './common.css',
+})
+export class Common {}
