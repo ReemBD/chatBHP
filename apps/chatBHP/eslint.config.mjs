ab5ec47b6a5c3e9f4c41a9890147a6b0a9dcb968
Reem Ben-David <reemile1996@gmail.com>
2025-08-11 11:49:36 +0300

    chore!: Change app prefix to bhp

--- apps/chatBHP/eslint.config.mjs
+++ apps/chatBHP/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'bhp',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'bhp',
           style: 'kebab-case',
         },
       ],
