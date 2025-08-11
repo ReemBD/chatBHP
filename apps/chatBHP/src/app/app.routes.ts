33d786be52fda6bc253791ce1beeee6b5944dccf
Reem Ben-David <reemile1996@gmail.com>
2025-08-11 12:53:36 +0300

    feat: Init Chat feature lib and connect to routes

--- apps/chatBHP/src/app/app.routes.ts
+++ apps/chatBHP/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        redirectTo: 'chat',
+        pathMatch: 'full',
+    },
+    {
+        path: 'chat',
+        loadComponent: () => import('@chat-bhp/chat/chat-feature').then(m => m.Chat),
+    }
+];
---
+++ libs/chat/chat-feature/src/chat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'bhp-chat-feature',
+  imports: [CommonModule],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.css',
+})
+export class Chat {}
