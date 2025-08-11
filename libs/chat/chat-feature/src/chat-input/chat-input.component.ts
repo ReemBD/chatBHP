3733aa75a32943e73b8a23213687747b305b0f94
Reem Ben-David <reemile1996@gmail.com>
2025-08-11 20:37:47 +0300

    Create initial chat-input

---
+++ libs/chat/chat-feature/src/chat-input/chat-input.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bhp-chat-input',
+  templateUrl: './chat-input.component.html',
+  standalone: true,
+})
+export class ChatInputComponent {
+}
