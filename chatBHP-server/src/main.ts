023e5da2702edff98c0f60b489fc58aa49fc71d4
Reem Ben-David <reemile1996@gmail.com>
2025-08-11 12:13:15 +0300

    chore: Setup cors for server

--- chatBHP-server/src/main.ts
+++ chatBHP-server/src/main.ts
@@ -9,6 +9,10 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // TODO: Enable CORS for the client only
+  app.enableCors({
+    origin: '*',
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
