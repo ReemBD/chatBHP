cfb2db3b57f026584af791696e42c6682d110cbf
Reem Ben-David <reemile1996@gmail.com>
2025-08-13 02:20:28 +0300

    chore: Build static files

--- chatBHP-server/src/main.ts
+++ chatBHP-server/src/main.ts
@@ -5,14 +5,19 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // TODO: Enable CORS for the client only
   app.enableCors({
     origin: '*',
   });
+  app.useStaticAssets(join(__dirname, 'static'));
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
---
+++ chatBHP-server/static/browser/chunk-Z236J2VX.js
@@ -0,0 +1,1 @@
+import{pb as a,qb as b}from"./chunk-TVS57YIB.js";export{a as Chat,b as USERNAME};
