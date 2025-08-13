66e0049e507f65483e0583d6265728bc86767d9b
Reem Ben-David <reemile1996@gmail.com>
2025-08-13 17:51:44 +0300

    fix(server): Remove wild card logic

--- chatBHP-server/src/app/app.controller.ts
+++ chatBHP-server/src/app/app.controller.ts
@@ -1,20 +1,6 @@
 
-import { Controller, Get, Req, Res, } from '@nestjs/common';
-import { join } from 'path';
-import { Request, Response } from 'express';
-
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get('*')
-  getIndex(@Req() req: Request, @Res() res: Response) {
-    if (
-      req.path.startsWith('/api') || // Skip API
-      req.path.includes('.')         // Skip files with extensions (assets)
-    ) {
-      return;
-    }
-
-    res.sendFile(join(__dirname, 'static/browser/index.html'));
-  }
 }
