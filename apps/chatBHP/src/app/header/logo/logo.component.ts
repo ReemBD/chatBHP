42222f238594e697556897c66b6bb418681fda31
Reem Ben-David <reemile1996@gmail.com>
2025-08-13 11:27:05 +0300

    feat(ui): Use theme colors for lotr

--- apps/chatBHP/src/app/header/logo/logo.component.ts
+++ apps/chatBHP/src/app/header/logo/logo.component.ts
@@ -1,8 +1,11 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'bhp-logo',
   templateUrl: './logo.component.html',
+  standalone: true,
+  imports: [UpperCasePipe],
 })
 export class LogoComponent {
 
