838798aea660d0b18334dc6088f7169b034e9e3a
Reem Ben-David <reemile1996@gmail.com>
2025-08-13 14:13:37 +0300

    chore: Move getRandomusername to chat

--- apps/chatBHP/src/app/app.config.ts
+++ apps/chatBHP/src/app/app.config.ts
@@ -8,12 +8,10 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { API_URL, SOCKET_URL } from '@chat-bhp/core/data-access';
 import { errorHandlingInterceptor } from '@chat-bhp/core/error-handler';
-import { USERNAME } from '@chat-bhp/chat/chat-feature';
 
 import { environment } from '../environments/environment';
 
 import { appRoutes } from './app.routes';
-import { getRandomUsername } from './utils/getRandomUsername';
 
 
 export const appConfig: ApplicationConfig = {
@@ -24,6 +22,5 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([errorHandlingInterceptor])),
     { provide: API_URL, useValue: environment.API_URL },
     { provide: SOCKET_URL, useValue: environment.SOCKET_URL },
-    { provide: USERNAME, useFactory: () => getRandomUsername() },
   ],
 };
--- libs/chat/chat-feature/src/tokens/username.token.ts
+++ libs/chat/chat-feature/src/tokens/username.token.ts
@@ -1,6 +1,9 @@
 import { InjectionToken } from "@angular/core";
+import { getRandomUsername } from "../utils/chat-user";
 
 /**
  * An arbitrary username for the chat.
  */
-export const USERNAME = new InjectionToken<string>('An arbitrary username for the chat.');
+export const USERNAME = new InjectionToken<string>('An arbitrary username for the chat.', {
+    factory: () => getRandomUsername(),
+});
