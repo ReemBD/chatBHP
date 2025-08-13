f2102f3d89d1193833ee567663908d1e22555561
Reem Ben-David <reemile1996@gmail.com>
2025-08-13 18:29:34 +0300

    feat(ui): Show username in header

--- apps/chatBHP/src/app/header/header.component.ts
+++ apps/chatBHP/src/app/header/header.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LogoComponent } from './logo/logo.component';
+import { USERNAME } from '@chat-bhp/chat/chat-feature';
 
 @Component({
   selector: 'bhp-header',
@@ -10,5 +11,5 @@ import { LogoComponent } from './logo/logo.component';
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-
+  readonly username = inject(USERNAME);
 }
